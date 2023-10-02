d2473f1e3352b6b3cb1e4823ff34cc30e34bae24
Sangyun Shin <sangyun0914@g.skku.edu>
2023-10-03 01:25:23 +0900

    Add file UI

--- src/app.py
+++ src/app.py
@@ -1,6 +1,11 @@
+import os
+from pathlib import Path
+
 import streamlit as st
 
 from chatbot import Chatbot
+
+FILE_SAVE_PATH = str(Path(__file__).parent.parent) + "/data/"
 
 
 @st.cache_resource
@@ -24,6 +29,9 @@ def main():
     for msg in st.session_state["messages"]:
         st.chat_message(msg["role"]).write(msg["content"])
 
+    if "files" not in st.session_state:
+        st.session_state["files"] = []
+
     # Chatbot
     Chatbot = load_model(model_name="gpt-4")
     if prompt := st.chat_input():
@@ -34,6 +42,28 @@ def main():
 
         st.session_state["messages"].append({"role": "assistant", "content": response})
         st.chat_message("assistant").write(response)
+
+    with st.sidebar:
+        active_files = st.file_uploader(
+            "채팅에 사용할 파일들.",
+            type=["png", "jpg", "jpeg", "pdf"],
+            accept_multiple_files=True,
+        )
+
+    # Save file to data directory
+    if active_files:
+        # Reset files list for one active session
+        st.session_state["files"] = []
+        if not os.path.exists(FILE_SAVE_PATH):
+            os.makedirs(FILE_SAVE_PATH)
+
+        for file in active_files:
+            file_path = FILE_SAVE_PATH + file.name
+            with open(file_path, "wb") as f:
+                f.write(file.getbuffer())
+
+            st.session_state["files"].append(file_path)
+            file.close()
 
 
 if __name__ == "__main__":
