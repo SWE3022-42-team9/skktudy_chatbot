2537d1f5c97a6328ebc03bcd780089e64c82cdf4
Sangyun Shin <sangyun0914@g.skku.edu>
2023-10-02 21:27:09 +0900

    Basic chatbot structure

--- src/app.py
+++ src/app.py
@@ -16,21 +16,21 @@ def main():
     st.title("💬 SKKTUDY CHATBOT")
     st.caption("🚀 기능 데모 프로그램")
     if "messages" not in st.session_state:
-        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+        st.session_state["messages"] = [{"role": "assistant", "content": "안녕하세요! 저는 SKKutor, 교육용 챗봇입니다.\n 어떤 도움이 필요하신가요?"}]
 
     for msg in st.session_state.messages:
         st.chat_message(msg["role"]).write(msg["content"])
 
-    uploaded_file = st.file_uploader("파일 업로드", type=['png', 'jpg', 'jpeg', 'pdf'])
+    # uploaded_file = st.file_uploader("파일 업로드", type=['png', 'jpg', 'jpeg', 'pdf'])
 
-    # Save file to data directory
-    if uploaded_file:
-        with open(FILE_SAVE_PATH + uploaded_file.name, "wb") as f:
-            f.write(uploaded_file.getbuffer())
-            files.append(FILE_SAVE_PATH + uploaded_file.name)
+    # # Save file to data directory
+    # if uploaded_file:
+    #     with open(FILE_SAVE_PATH + uploaded_file.name, "wb") as f:
+    #         f.write(uploaded_file.getbuffer())
+    #         files.append(FILE_SAVE_PATH + uploaded_file.name)
 
     # Chatbot
-    Chatbot = load_model(model_name='gpt-3.5-turbo')
+    Chatbot = load_model(model_name='gpt-4')
     if prompt := st.chat_input():
         st.session_state.messages.append({"role": "user", "content": prompt})
         st.chat_message("user").write(prompt)
--- src/chatbot.py
+++ src/chatbot.py
@@ -1,4 +1,5 @@
 from typing import Any, Optional, Type
+from typing import Any, List, Optional, Sequence, Tuple
 
 import openai
 from dotenv import load_dotenv
@@ -12,8 +13,63 @@ from langchain.schema import AIMessage, HumanMessage, SystemMessage
 from langchain.tools import BaseTool, StructuredTool, Tool, tool
 from langchain.utilities import SerpAPIWrapper
 from langchain.document_loaders import PyPDFLoader
+from langchain.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.agents import AgentExecutor
+# from langchain.agents.output_parsers import JSONAgentOutputParser
+from output_parsers import JSONAgentOutputParser
+from langchain.agents.format_scratchpad import format_log_to_messages
+
+from langchain.agents.conversational_chat.output_parser import ConvoOutputParser
+
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import AgentAction, BaseOutputParser, BasePromptTemplate
+
+from langchain.chains import LLMChain
+from templates import (
+    PREFIX,
+    SUFFIX,
+    TEMPLATE_TOOL_RESPONSE,
+    FORMAT_INSTRUCTIONS,
+)
 
 load_dotenv()
+
+base_prompt = ChatPromptTemplate(
+    messages=[
+        SystemMessagePromptTemplate.from_template(PREFIX),
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{question}")
+    ]
+)
+
+memory = ConversationBufferMemory(memory_key="chat_history",return_messages=True)
+
+def _handle_error(error) -> str:
+    return str(error)[:50]
+
+def summarize_template(query: str) -> str:
+    template = ChatPromptTemplate.from_messages(
+        [
+            SystemMessage(
+                content=(
+                    """You are a helpful assistant that summarizes a document.
+                    Give clear and concise summaries of the document."""
+                )
+            ),
+            HumanMessagePromptTemplate.from_template("{text}"),
+        ]
+    )
+
 
 
 class Chatbot:
@@ -21,23 +77,83 @@ class Chatbot:
         self,
         model_name: str = "gpt-3.5-turbo",
     ):
-        self.chatbot = ChatOpenAI(model_name=model_name)
+        chat_model = ChatOpenAI(model_name=model_name, temperature=0)
+
+        # self.summaryChain = LLMChain(llm=self.llm, prompt="Prompt")
+
+        chatbot = LLMChain(
+            llm=chat_model,
+            prompt=base_prompt,
+            verbose=True,
+        )
+        tools = [
+            Tool(
+                name="Summary",
+                func=chatbot.run,
+                description="useful only when you need summarize a given pdf document",
+                return_direct=True,
+            )
+        ]
+
+
+        prompt = self.create_prompt(
+            tools,
+            system_message=PREFIX,
+            human_message=SUFFIX,
+            input_variables=None,
+            output_parser=ConvoOutputParser()
+        )
+        chat_model_with_stop = chat_model.bind(stop=["\nObservation"])
+
+
+        agent = {
+            "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_log_to_messages(x['intermediate_steps'], template_tool_response=TEMPLATE_TOOL_RESPONSE),
+            "chat_history": lambda x: x["chat_history"],
+        } | prompt | chat_model_with_stop | JSONAgentOutputParser()
+
+
+        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+        self.agent = AgentExecutor(agent=agent, tools=tools, verbose=True, memory=memory)
+
+
 
     def __call__(
         self,
-        messages: list[AIMessage], 
+        messages: list[HumanMessage], 
         files: Optional[list[str]] = None,
         **kwargs: Any
     ) -> Any:
-        pass
+        ret = self.agent.invoke({'input': messages})['output']
+        print(ret)
+        return ret
 
-
-def chat(prompt):
-    chatbot = ChatOpenAI(temperature=0)
-    ret = chatbot([HumanMessage(content=prompt)])
-    ret = ret[0].content
-
-    
-
-
-    return ret
+    @classmethod
+    def create_prompt(
+        self,
+        tools: Sequence[BaseTool],
+        system_message: str = PREFIX,
+        human_message: str = SUFFIX,
+        input_variables: Optional[List[str]] = None,
+        output_parser: Optional[BaseOutputParser] = None,
+    ) -> BasePromptTemplate:
+        tool_strings = "\n".join(
+            [f"> {tool.name}: {tool.description}" for tool in tools]
+        )
+        tool_names = ", ".join([tool.name for tool in tools])
+        _output_parser = output_parser
+        format_instructions = human_message.format(
+            format_instructions=FORMAT_INSTRUCTIONS
+        )
+        final_prompt = format_instructions.format(
+            tool_names=tool_names, tools=tool_strings
+        )
+        if input_variables is None:
+            input_variables = ["input", "chat_history", "agent_scratchpad"]
+        messages = [
+            SystemMessagePromptTemplate.from_template(system_message),
+            MessagesPlaceholder(variable_name="chat_history"),
+            HumanMessagePromptTemplate.from_template(final_prompt),
+            MessagesPlaceholder(variable_name="agent_scratchpad"),
+        ]
+        return ChatPromptTemplate(input_variables=input_variables, messages=messages)
